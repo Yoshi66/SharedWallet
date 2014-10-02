d65b24cc02830505081b6854889bb20e08ee2c57
Yoshiki <yoshiki.mc@gmail.com>
2014-10-01 23:36:56 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SWP::Application.config.secret_key_base = 'cec4495e2644586bcf45be0dc45c6b2faca62627ec4886d8e58ecf361b16dabdfde1467259e053fe845b8c35e2e36f185a8eb4419bfb010574d18ac2633733ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SWP::Application.config.session_store :cookie_store, key: '_SWP_session'
