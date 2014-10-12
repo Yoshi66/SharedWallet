d9c99ee1ad240ffe8c12c56276a6135f191e360e
Yoshiki <yoshiki.mc@gmail.com>
2014-10-12 16:01:32 -0700

    change redirect url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   def home
@@ -28,15 +29,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if@user.save
+      flash[:success] = "Welcome to Shared Wallet!"
+      redirect_to("http://localhost:3000/projects")
+    else
+      render 'new'
     end
   end
 
@@ -72,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email,:password, :password_confirmation)
     end
 end
